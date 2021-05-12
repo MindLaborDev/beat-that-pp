90f932b5ca22d6a698d1b95c42f47b34e11160e8
Samuel Braun <42311986+MindLaborDev@users.noreply.github.com>
2021-05-12 19:52:28 +0200

    Added core analysing algorithm

--- map/index.js
+++ map/index.js
@@ -8,6 +8,8 @@ let difficulty;
 let audio;
 let data;
 let map;
+let chart;
+let analysedBeatmap;
 $(document).ready(async function () {
 
     const args = location.hash.split(",");
@@ -40,6 +42,8 @@ $(document).ready(async function () {
     data = await decodeZippedMap(blob, zip.endsWith(".audica"));
     console.log(data);
 
+    analysedBeatmap = new BeatMap(data.mapData, data.infos._beatsPerMinute);
+
     // Show song informations
     renderSongHero({
         cover: `https://beatsaver.com${map.coverURL}`,
@@ -54,8 +58,83 @@ $(document).ready(async function () {
     renderDifficultyMenu(data.beatmap);
     renderBasicMapInfos();
 
+    const npmHistory = getNPMHistory();
+    setupCharts(npmHistory);
+
 })
 
+
+function getNPMHistory() {
+
+    let historyItems = [];
+    let lastSongChunkIndex = Infinity;
+    let currentNoteCount = 0;
+
+    for (const note of analysedBeatmap.notes) {
+        let currentSongChunkIndex = ~~(note._time * 60 / analysedBeatmap.bpm / 5);
+
+        if (lastSongChunkIndex !== currentSongChunkIndex) {
+            lastSongChunkIndex = currentSongChunkIndex;
+            historyItems.push(currentNoteCount / 5);
+            currentNoteCount = 1;
+            continue;
+        }
+
+        currentNoteCount++;
+    }
+
+    return historyItems;
+}
+
+
+function setupCharts(npmHistory) {
+
+    let seconds = 0;
+    let labels = [];
+    for (const npmItem of npmHistory) {
+        seconds += 5;
+        labels.push(seconds + "s");
+    }
+    
+    const ctx = document.getElementById('chart');
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels,
+            datasets: [{
+                label: 'Notes per second',
+                data: npmHistory,
+                backgroundColor: '#f03d4d',
+                borderColor: '#f03d4d',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            elements: {
+                line: {
+                    tension: 0.2,
+                    cubicInterpolationMode: "monotone"
+                }
+            },
+            normalized: true,
+            animation: false
+        }
+    });
+
+    $("#chart-wrapper").removeClass("u-none");
+}
+
+function rerenderCharts() {
+    analysedBeatmap = new BeatMap(data.mapData, data.infos._beatsPerMinute);
+    const npmHistory = getNPMHistory();
+    chart.data.datasets[0].data = npmHistory;
+    chart.update();
+}
 
 function renderBasicMapInfos() {
 
@@ -74,7 +153,7 @@ function renderBasicMapInfos() {
 
     $("#map-general-info-wrapper").html(`
         <!-- https://cdn.wes.cloud/beatstar/bssb/v2-all.json -->
-        Version <span>${data.infos._version}</span> <span class="date pl-1">(${convertDate(map.uploaded)})</span><br />
+        Version <span>${convertDate(map.uploaded)}</span> <span class="date pl-1">(${data.infos._version})</span><br />
         ${requires}<br />
 
         <div id="map-general-infos">
@@ -154,6 +233,8 @@ async function selectDifficulty(difficultyRank) {
 
     $("#difficulty-menu li").removeClass("selected");
     $(`#difficulty-menu li[data-did="${difficulty}"]`).addClass("selected");
+
+    rerenderCharts();
 }
 
 
@@ -401,3 +482,272 @@ function copyTextToClipboard(text) {
         console.error('Async: Could not copy text: ', err);
     });
 }
+
+
+
+class Saber {
+
+    constructor(initialChunk) {
+
+        // Set starting position
+        this.position = BeatMap.getNoteBehind(initialChunk[0]._lineIndex, initialChunk[0]._lineLayer, initialChunk[0]._cutDirection);
+        
+    }
+
+
+    traverse(chunk) {
+        this.position = chunk.path[chunk.path.length - 1];
+    }
+
+
+}
+
+class BeatMap {
+
+    constructor(rawData, bpm) {
+        this.bpm = bpm;
+        this.notes = rawData._notes.filter(n => n._type !== 3);
+        this.leftNotes = this.notes.filter(n => n._type === 0);
+        this.rightNotes = this.notes.filter(n => n._type === 1);
+        this.bombs = rawData._notes.filter(n => n._type === 3);
+
+        this.chunkedDataLeft = this.normalize(this.leftNotes);
+        this.chunkedDataRight = this.normalize(this.rightNotes);
+
+        const rightSaber = new Saber(this.chunkedDataRight[0]);
+        const rightSaberPath = this.determineSaberPath(rightSaber);
+
+        const leftSaber = new Saber(this.chunkedDataLeft[0]);
+        const leftSaberPath = this.determineSaberPath(leftSaber);
+
+        //console.log(rightSaberPath);
+    }
+
+
+    determineSaberPath(saber) {
+        let saberPath = [saber.position];
+
+        for (const chunk of this.chunkedDataRight) {
+            if (saberPath.length === 1)
+                saberPath[0].time = 0;
+            
+            const orderedChunk = this.orderChunk(chunk, saber.position);
+            saber.traverse(orderedChunk);
+            orderedChunk.path.shift();
+            saberPath = saberPath.concat(orderedChunk.path);
+        }
+
+        return saberPath;
+    }
+
+    orderChunk(chunk, saberPosition) {
+
+        //console.log("Chunk");
+
+        let minAngle = Infinity;
+        let bestPermutation, bestTraversal;
+        const chunkPermutations = this.permutations(chunk);
+        for (const permutation of chunkPermutations) {
+            //console.log(permutation[0]._time);
+            //console.log(permutation.map(n => `${n._lineIndex} : ${n._lineLayer} (${n._cutDirection})`));
+
+            const position = saberPosition || {
+                x: permutation[0]._lineIndex,
+                y: permutation[0]._lineLayer,
+            };
+
+            const traversalResults = this.traverseChunk(permutation, position);
+            //console.log(traversalResults);
+            //console.log();
+            if (minAngle > traversalResults.sumAngle) {
+                minAngle = traversalResults.sumAngle;
+                bestTraversal = traversalResults.traversal;
+                bestPermutation = permutation;
+            }
+        }
+
+        return {
+            angle: minAngle,
+            chunk: bestPermutation,
+            path: bestTraversal
+        };
+    }
+
+
+    traverseChunk(chunk, startingPosition) {
+
+        const traversal = [{
+            x: startingPosition.x,
+            y: startingPosition.y
+        }];
+        for (const note of chunk) {
+
+            // If this is the first note add time to starting position
+            if (traversal.length === 1) {
+                traversal[0].time = BeatMap.beatToTime(note._time);
+            }
+
+            traversal.push({
+                x: note._lineIndex,
+                y: note._lineLayer,
+                time: BeatMap.beatToTime(note._time)
+            });
+
+            if (note._cutDirection === 8)
+                continue;
+
+            // Append the position after the current note
+            const after = this.getNoteAfter(note._lineIndex, note._lineLayer, note._cutDirection);
+            traversal.push({
+                x: after.x,
+                y: after.y,
+                time: BeatMap.beatToTime(note._time)
+            });
+        }
+
+        let lastTravPos;
+        let uniqueTraversal = [];
+        for (const pos of traversal) {
+
+            if (lastTravPos != null && pos.x === lastTravPos.x && pos.y === lastTravPos.y)
+                continue;
+
+            uniqueTraversal.push(pos);
+            lastTravPos = pos;
+        }
+
+        let totalAngleChanges = 0;
+        for (let i = 0; i < uniqueTraversal.length - 2; i++) {
+            const dXA = uniqueTraversal[i + 1].x - uniqueTraversal[i].x;
+            const dYA = uniqueTraversal[i + 1].y - uniqueTraversal[i].y;
+            const dXB = uniqueTraversal[i + 2].x - uniqueTraversal[i + 1].x;
+            const dYB = uniqueTraversal[i + 2].y - uniqueTraversal[i + 1].y;
+            const dA = this.getAngleBetweenVectors(dXA, dYA, dXB, dYB);
+            totalAngleChanges += dA;
+        }
+
+        return {
+            traversal: uniqueTraversal,
+            sumAngle: totalAngleChanges < 0.001 ? 0 : totalAngleChanges
+        }
+    }
+
+
+    getAngleBetweenVectors(x1, y1, x2, y2) {
+        return Math.acos((x1 * x2 + y1 * y2) / (Math.sqrt(x1 * x1 + y1 * y1) * Math.sqrt(x2 * x2 + y2 * y2)));
+    }
+
+
+    /**
+     * Returns the position of saber before hitting the block (behind the block)
+     * Its the position where the saber needs to go to make a good cut
+     */
+    static getNoteBehind(x, y, dir) {
+        switch (dir) {
+            case 0: return {
+                x, y: y - 1
+            }
+            case 1: return {
+                x, y: y + 1
+            }
+            case 2: return {
+                x: x + 1, y
+            }
+            case 3: return {
+                x: x - 1, y
+            }
+            case 4: return {
+                x: x + 1, y: y - 1
+            }
+            case 5: return {
+                x: x - 1, y: y - 1
+            }
+            case 6: return {
+                x: x + 1, y: y + 1
+            }
+            case 7: return {
+                x: x - 1, y: y + 1
+            }
+            default: return {
+                x, y
+            }
+        }
+    }
+
+
+    getNoteAfter(x, y, dir) {
+        switch (dir) {
+            case 0: return {
+                x, y: y + 1
+            }
+            case 1: return {
+                x, y: y - 1
+            }
+            case 2: return {
+                x: x - 1, y
+            }
+            case 3: return {
+                x: x + 1, y
+            }
+            case 4: return {
+                x: x - 1, y: y + 1
+            }
+            case 5: return {
+                x: x + 1, y: y + 1
+            }
+            case 6: return {
+                x: x - 1, y: y - 1
+            }
+            case 7: return {
+                x: x + 1, y: y - 1
+            }
+            default: return {
+                x, y
+            }
+        }
+    }
+
+
+    /**
+     * Returns all permutation of a given array (without repetition)
+     */
+    permutations(xs) {
+        let ret = [];
+
+        for (let i = 0; i < xs.length; i = i + 1) {
+            let rest = this.permutations(xs.slice(0, i).concat(xs.slice(i + 1)));
+
+            if (!rest.length)
+                ret.push([xs[i]])
+            else
+                for (let j = 0; j < rest.length; j = j + 1)
+                    ret.push([xs[i]].concat(rest[j]))
+        }
+        return ret;
+    }
+
+
+    normalize(notes) {
+        let lastNoteTime = 0;
+        let chunk = [];
+        let chunks = [];
+        for (const note of notes) {
+            if (note._time !== lastNoteTime && chunk.length > 0) {
+                chunks.push(chunk);
+                chunk = [];
+            }
+            chunk.push(note);
+            lastNoteTime = note._time;
+        }
+        return chunks;
+    }
+
+
+    static beatToTime(beat) {
+        //console.log(this.bpm);
+        return +beat * 60 / +this.bpm;
+    }
+
+
+}
+
